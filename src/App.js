c44900350021a757eecc3db30e6793e5b9d37361
Brandon <brandon121j@gmail.com>
2021-10-28 12:22:28 -0400

    checkpoint commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello</h1>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="header">
+          <h1>Brandon Johnson</h1>
+          <h3>header</h3>
+        </div>
+				<div className="sidebar">
+          <h3>Sidebar</h3>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+				<div className="body">
+					body
+					<div className="box">box</div>
+				</div>
+				<div className="footer">footer</div>
+			</div>
+		);
+	}
 }
