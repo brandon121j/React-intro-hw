193364c144abfd24c855456eb38f88d1776efe6d
Brandon <brandon121j@gmail.com>
2021-10-28 19:02:20 -0400

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,99 @@
 import React, { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
+import pic1 from "./components/mooninites.png";
+import pic2 from "./components/IMG_4232.jpg";
+import pic3 from "./components/IMG_4263.jpg";
+import pic4 from "./components/IMG_4005.jpg";
 import "./App.css";
 
 export default class App extends Component {
+
+  state = {
+    currentColor: ""
+  }
+
+  clickHandler = (e) => {
+    this.setState({
+      currentColor: e.target.value
+    })
+    console.log(this.state.currentColor)
+  }
+
 	render() {
 		return (
-			<div className="container">
+            <div className="container">
 				<div className="header">
 					<h1>Brandon Johnson</h1>
-					<h3>header</h3>
 				</div>
-				<div className="sidebar">
-					<h3>Sidebar</h3>
+				<div className="sidebar"
+          style={{ backgroundColor: this.state.currentColor }}
+        >
 					<ul>
 						<li>
-							<button>Gray</button>
+							<button id="buttonGreen" onClick={this.clickHandler} value="green">Green</button>
 						</li>
 						<li>
-							<button>Blue</button>
+							<button id="buttonBlue" onClick={this.clickHandler} value="blue">Blue</button>
 						</li>
 						<li>
-							<button>Red</button>
+							<button id="buttonRed" onClick={this.clickHandler} value="red">Red</button>
 						</li>
 					</ul>
 				</div>
 				<div className="body">
-					<h1>body</h1>
-          <div className="bodyContainer">
-            <div className="bodyBox"></div>
-            <div className="bodyBox"></div>
-            <div className="bodyBox"></div>
+					<div className="bodyTopContainer">
+						<img src={pic1} alt="Mooninite" title="Mooninites" />
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis turpis
+							turpis. Nam hendrerit, nulla nec faucibus posuere, mauris nulla venenatis
+							orci, vel ultrices massa orci eget magna. Maecenas ullamcorper augue
+							maximus neque posuere bibendum. Nullam ligula tortor, scelerisque sit
+							amet purus sit amet, lacinia molestie nunc. Sed ac nibh vitae est
+							ultricies placerat. Ut et enim dapibus, finibus ipsum nec, dignissim
+							quam. Nulla rutrum, nulla quis porttitor ornare, orci nisi vehicula erat,
+							vel rhoncus quam nisl eu orci. Ut eu dapibus nunc. Cras vitae dignissim
+							augue. Etiam faucibus accumsan ullamcorper.
+						</p>
+					</div>
+					<div className="bodyContainer">
+						
+					<div className="box">
+          <div className="bodyBoxContainer">
+							<div className="bodyBox">
+								<img src={pic2} alt="Joe" title="Joe" />
+								<p>
+									In magna dolor, finibus id metus ut, efficitur fringilla massa. Nunc
+									porta interdum libero nec dapibus. Integer pulvinar magna nec elementum
+									blandit. Praesent porta accumsan interdum. Pellentesque condimentum
+									neque tellus, quis lacinia nisl
+								</p>
+							</div>
+							<div className="bodyBox">
+								<img src={pic3} alt="You aren't that guy pal meme" title="You aren't that guy pal meme" />
+								<p>
+									Nullam nec lectus eu tellus aliquam suscipit id sed metus. Interdum et
+									malesuada fames ac ante ipsum primis in faucibus. Maecenas viverra
+									consectetur ipsum, efficitur pharetra massa. Mauris sit amet
+									pellentesque dui. Integer eleifend dui 
+								</p>
+							</div>
+							<div className="bodyBox">
+								<img src={pic4} alt="Drake" title="Drake album cover"/>
+								<p>
+									Nam consequat blandit justo, nec accumsan dolor scelerisque id.
+									Maecenas at augue ac justo posuere dapibus sed a metus. Mauris
+									imperdiet ipsum quis tortor imperdiet, vel consectetur mi ornare.
+									Curabitur elementum est vitae elit aliquam vehicula id cursus ipsum.
+								</p>
+							</div>
+						</div>
+					</div>
           </div>
-					<div className="box">box</div>
 				</div>
-				<div className="footer">footer</div>
+				<div className="footer">
+          <p>&copy;Copyright </p>
+          <p>Brandon Johnson</p>
+				</div>
 			</div>
-		);
+        );
 	}
 }
