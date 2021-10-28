33518db6c973a4ad7862cb6f13ed3a089e39205a
Brandon <brandon121j@gmail.com>
2021-10-28 14:23:05 -0400

    Checkpoint commit

--- src/App.js
+++ src/App.js
@@ -7,19 +7,30 @@ export default class App extends Component {
 		return (
 			<div className="container">
 				<div className="header">
-          <h1>Brandon Johnson</h1>
-          <h3>header</h3>
-        </div>
+					<h1>Brandon Johnson</h1>
+					<h3>header</h3>
+				</div>
 				<div className="sidebar">
-          <h3>Sidebar</h3>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
+					<h3>Sidebar</h3>
+					<ul>
+						<li>
+							<button>Gray</button>
+						</li>
+						<li>
+							<button>Blue</button>
+						</li>
+						<li>
+							<button>Red</button>
+						</li>
+					</ul>
+				</div>
 				<div className="body">
-					body
+					<h1>body</h1>
+          <div className="bodyContainer">
+            <div className="bodyBox"></div>
+            <div className="bodyBox"></div>
+            <div className="bodyBox"></div>
+          </div>
 					<div className="box">box</div>
 				</div>
 				<div className="footer">footer</div>
